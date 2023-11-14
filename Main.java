b366f3fc9a8f221a318b096104f4e525fe4f0217
Emirhanulkerr <emirhanulkerr@icloud.com>
2023-11-14 19:16:38 +0300

    First commit
    All done!

---
+++ Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int boardWidth= 700;
+        int boardHeight=boardWidth;
+
+        JFrame frame= new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setBackground(Color.BLACK);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame= new SnakeGame(boardWidth,boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+    }
+}
